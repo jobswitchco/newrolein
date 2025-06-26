a2aab37b297e98fd157b58b8cf26ceebdbbef70d
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-06-27 00:04:07 +0530

    26 June 2025 v0.2

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import dbConnection from "./db.js";
+import dbConnection from "./db.mjs";
 const app = express();
 import bodyParser from "body-parser";
 import cors from 'cors';
