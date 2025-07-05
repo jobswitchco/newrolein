cbc20845cbcd32c75c45b6715f481dfd2f8351d2
bhaskarsriramp <bhaskarsriram.p@gmail.com>
2025-07-06 00:48:23 +0530

    05 July 2025 v0.2

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import App from './App.js';
 import store from './store/store.js';
 import reportWebVitals from './reportWebVitals.js';
 import { Provider } from 'react-redux';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // Target the root element
 const rootElement = document.getElementById('root');
 
 // Wrap the app in GoogleOAuth and Redux Provider
 const AppTree = (
-  <GoogleOAuthProvider clientId='455976777846-jve7vqhe2ujq5ofm8svqkbpj8mse8kf9.apps.googleusercontent.com'>
     <Provider store={store}>
       <App />
     </Provider>
-  </GoogleOAuthProvider>
 );
 
 // 🔁 Use hydrateRoot for SSR (used by react-snap), else fallback to createRoot
